5f3959c4b1edc1979013a91005fb918c0f44cd5a
kujenga <ataylor0123@gmail.com>
2017-09-27 15:50:33 -0400

    add editorconfig file for home dir

--- sync.sh
+++ sync.sh
@@ -7,7 +7,7 @@ set -e
 R="$(dirname "$0")"
 
 # files that are identical between here and the local copy
-HOME_FILES='.vimrc .gitignore_global'
+HOME_FILES='.vimrc .gitignore_global .editorconfig'
 
 for f in $HOME_FILES; do
 	cp "$HOME/$f" "$R/home/$f";

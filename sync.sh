77d55725f420be4c3de5a784da1b47e0a0860c72
Aaron Taylor <aaron.taylor@salesforce.com>
2023-04-05 23:12:01 -0400

    sync: add bi-directional functionality
    This allows syncing TO the home directory for new machines,
    eliminating the manual step of copying and renaming otherwise
    that would be needed.

--- sync.sh
+++ sync.sh
@@ -7,16 +7,45 @@ set -e
 
 R="$(dirname "$0")"
 
+DIRECTION='in'
+
+while getopts 'out:h' opt; do
+  case "$opt" in
+    o)
+      echo "Syncing files out to home directory..."
+      DIRECTION='out'
+      ;;
+
+    ?|h)
+      echo "Usage: $(basename $0) [-a] [-b] [-c arg]"
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
 # Sync all files in the repo home/ directory already.
 for f in $(find ./home -type f -not -name ".*" -not -name "*.md" | cut -c8-); do
     # Copy the "." version from the home directory. We omit the "." here to
     # make the times in the repo easier to work with.
-    src="$HOME/.$f"
-    dst="$R/home/$f"
-    if [ -f "$src" ]; then
-        echo "Syncing: $src -> $dst"
-        cp "$src" "$dst"
-    else
-        echo "Skipping sync for: $dst"
-    fi
+    live="$HOME/.$f"
+    store="$R/home/$f"
+
+    case "$DIRECTION" in
+        out)
+            echo "Syncing: $store -> $live"
+	    mkdir -p "$(dirname "$live")"
+            cp "$store" "$live"
+	    ;;
+
+        in)
+            if [ -f "$live" ]; then
+                echo "Syncing: $live -> $store"
+                cp "$live" "$store"
+            else
+                echo "Skipping sync for: $store"
+            fi
+	    ;;
+
+    esac
 done

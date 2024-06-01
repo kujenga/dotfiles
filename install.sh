1f22251aff9c1e7ee8097810ac5786427f2298e0
Aaron Taylor <aaron@additive.ai>
2024-05-31 17:15:06 -0700

    install: lighter weight defaults for codespaces
    This drops some of the heavier-weight installs that are not needed
    in all dev container environments, hiding them behind an env var.

--- install.sh
+++ install.sh
@@ -2,62 +2,77 @@
 #
 # This script installs configuration onto the current machine.
 
-set -euxo pipefail
+set -exo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Install Homebrew
+
+# Function definitions
+
+# Homebrew for macos
 # https://brew.sh/
-if [[ $(command -v brew) == "" ]]; then
-    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
+install_brew() {
+    if [[ $(command -v brew) == "" ]]; then
+        NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+        echo "brew already installed: $(which brew)"
+    fi
+}
 
-# Install oh-my-zsh
-# https://ohmyz.sh/#install
-if [ ! -e ~/.oh-my-zsh ]; then
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
-
-# Install Rust
-if [[ $(command -v rustup) == "" ]]; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-fi
-
-# Platform specific setup
-case $(uname) in
-Darwin)
-    # Install Homebrew: https://github.com/Homebrew/install
-    type brew >/dev/null 2>&1 || { echo >&2 "I require Homebrew but it's not installed.  Aborting."; exit 1; }
-
-    # NOTE: This is currently disabled because it does not seem to be needed on
-    # the latest macs for most use cases, but kept around for reference if
-    # needed in the future.
-    #
-    # Based on: https://wilsonmar.github.io/maximum-limits/ except we only
-    # configure mac files, since max proc defaults seem reasonable.
-    # echo 'Configuring file limits'
-    # sudo cp "$DIR/macos/limit.maxfiles.plist" "/Library/LaunchDaemons/limit.maxfiles.plist"
-    # sudo launchctl load -w /Library/LaunchDaemons/limit.maxfiles.plist
-
-    ;;
-
-Linux)
-    type apt-get >/dev/null 2>&1 || { echo >&2 "I require apt-get but it's not installed.  Aborting."; exit 1; }
-
-    # Install homebrew on linux
-    # https://docs.brew.sh/Homebrew-on-Linux
+# Homebrew on linux
+# https://docs.brew.sh/Homebrew-on-Linux
+install_linuxbrew() {
+    install_brew
     test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
     test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+}
 
-    ;;
-esac
+# oh-my-zsh
+# https://ohmyz.sh/#install
+install_oh_my_zsh() {
+    if [ ! -e ~/.oh-my-zsh ]; then
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    else
+        echo "oh-my-zsh already installed"
+    fi
+}
+
+# Rust
+install_rustup() {
+    if [[ $(command -v rustup) == "" ]]; then
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    else
+        echo "rustup already installed: $(which rustup)"
+    fi
+}
+
+
+# Install Process
+
+# Additional non-default installs
+if [ "${INSTALL_ALL}" = "yes" ]; then
+    # Platform specific setup
+    case $(uname) in
+    Darwin)
+        install_brew
+        ;;
+
+    Linux)
+        install_linuxbrew
+        ;;
+    esac
+
+    install_rustup
+
+    # Install the brew bundle for my preferences.
+    brew bundle --file="$DIR/Brewfile"
+fi
+
+install_oh_my_zsh
 
 # Sync configuration files
-$DIR/sync.sh -o
-
-# Install the brew bundle for my preferences.
-brew bundle --file="$DIR/Brewfile"
+"${DIR}/sync.sh" -o
 
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
